1b8661634f8802bb0317fe577cd58d97baa90dfb
孙道然 <513088699@qq.com>
2016-09-30 15:40:49 +0800

    Initial Commit

---
+++ GestureImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GestureImageView
+//
+//  Created by sundaoran on 16/9/30.
+//  Copyright © 2016年 sundaoran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GestureImageView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GestureImageView
+//
+//  Created by sundaoran on 16/9/30.
+//  Copyright © 2016年 sundaoran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
